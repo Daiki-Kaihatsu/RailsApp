73bdc0011c9e22f7db530a13ac2398a6a61a651f
Daiki-Kaihatsu <daichan.kaihatsu@gmail.com>
2020-07-18 19:28:08 +0900

    CSSでログイン画面と新規登録画面を整えた

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   root 'homes#index'
+  resources :users
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#new_guest'
   end
